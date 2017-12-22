56b93ad07d1e0b75142557b37ff67957acac3ad0
jibonbiswas007 <33145139+jibonbiswas007@users.noreply.github.com>
2017-12-22 14:58:06 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,18 @@
+(function($) {
+    "use strict";
+    
+    jQuery(document).ready(function($){
+    	$('.hover').hover(function(){
+			$(this).addClass('flip');
+		},function(){
+			$(this).removeClass('flip');
+		});
+        
+    });
+    
+    jQuery(window).load(function(){
+        
+    });
+    
+    
+}(jQuery));
